854ee8adfece5ffa6694c3cbcf6fbafc8622647b
Frank Lyder Bredland <fl@nyggen.com>
2024-04-28 18:29:24 +0200

    Beginning of lua example

--- hydra.lua
+++ hydra.lua
@@ -1,1 +1,11 @@
-print("test");
+print("Hello world from lua!");
+
+h = Handler();
+
+h.add_handler();
+
+m = Manifest();
+
+m.add_dependency(--[[ "http://curl.haxx.se/download/curl-7.42.1.zip" ]]);
+m.add_dependency(--[[ "https://github.com/philsquared/Catch/archive/master.zip" ]]);
+m.add_dependency(--[[ "https://github.com/akheron/jansson/archive/master.zip" ]]);
