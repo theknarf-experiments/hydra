beda031e7cfe36640892111c519a77036c2f72a5
Frank Lyder Bredland <fl@nyggen.com>
2024-04-29 10:52:42 +0200

    ci: pipeline for deploying `docs/` to Github Pages

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,4 +3,12 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.jsx'
 });
 
-module.exports = withNextra();
+module.exports = withNextra({
+	basePath: '/hydra',
+	assetPrefix: '/hydra/',
+
+	output: 'export',
+	images: {
+		unoptimized: true,
+	},
+});
